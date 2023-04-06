c180bda42d7790e19be266a4d09cadfe0f947a63
Yudai Kitano <36891892+ktnydi@users.noreply.github.com>
2023-04-07 00:13:27 +0900

    chore: ディレクトリ構成を整理

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:github_app/main.dart';
+import 'package:github_app/presentation/screens/app/app_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
