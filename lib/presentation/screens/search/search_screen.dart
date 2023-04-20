e07eaffd54038380d34e46f4de4d88fb642eb2a8
Yudai Kitano <36891892+ktnydi@users.noreply.github.com>
2023-04-20 23:23:51 +0900

    feat: 検索フォームのデザインを変更

--- lib/presentation/screens/search/search_screen.dart
+++ lib/presentation/screens/search/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:github_app/extensions/build_context.dart';
 import 'package:github_app/presentation/screens/repository/repository_screen.dart';
 import 'package:github_app/presentation/widgets/repository_tile.dart';
 
@@ -11,74 +12,79 @@ class SearchRepositoriesScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final viewPadding = MediaQuery.of(context).viewPadding;
     final searchResult = ref.watch(searchRepositories);
     final searchKeywordState = ref.watch(searchKeywordProvider);
 
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(16, 16 + viewPadding.top, 16, 0),
-            child: const SearchField(),
-          ),
-          const SizedBox(height: 16),
-          if (searchKeywordState.isNotEmpty)
-            Expanded(
-              child: searchResult.when(
-                data: (data) {
-                  if (data!.isEmpty) {
-                    return const Center(
-                      child: Text('一致する結果が見つかりませんでした。'),
-                    );
-                  }
-
-                  return ListView.separated(
-                    padding: EdgeInsets.zero,
-                    itemBuilder: (context, index) {
-                      final repository = data[index];
-
-                      return RepositoryTile(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  RepositoryScreen(id: repository.id),
-                            ),
-                          );
-                        },
-                        repository: repository,
-                      );
-                    },
-                    separatorBuilder: (_, __) => const Divider(
-                      height: 1,
-                      indent: 16,
-                      endIndent: 16,
-                    ),
-                    itemCount: data.length,
-                  );
-                },
-                error: (error, stack) {
-                  return Center(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: const [
-                        Icon(Icons.error_outline),
-                        SizedBox(height: 16),
-                        Text('エラーが発生しました。'),
-                      ],
-                    ),
-                  );
-                },
-                loading: () {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                },
+      appBar: AppBar(
+        surfaceTintColor: Colors.transparent,
+        title: const SearchField(),
+      ),
+      body: searchResult.when(
+        data: (data) {
+          if (searchKeywordState.isEmpty) {
+            return Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.search,
+                    size: 80,
+                    color: context.colorScheme.surfaceVariant,
+                  ),
+                  const Text('リポジトリを検索してください。'),
+                ],
               ),
+            );
+          }
+
+          if (data!.isEmpty) {
+            return const Center(
+              child: Text('一致する結果が見つかりませんでした。'),
+            );
+          }
+
+          return ListView.separated(
+            itemBuilder: (context, index) {
+              final repository = data[index];
+
+              return RepositoryTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RepositoryScreen(id: repository.id),
+                    ),
+                  );
+                },
+                repository: repository,
+              );
+            },
+            separatorBuilder: (_, __) => const Divider(
+              height: 1,
+              indent: 16,
+              endIndent: 16,
             ),
-        ],
+            itemCount: data.length,
+          );
+        },
+        error: (error, stack) {
+          return Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Icon(Icons.error_outline),
+                SizedBox(height: 16),
+                Text('エラーが発生しました。'),
+              ],
+            ),
+          );
+        },
+        loading: () {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
--- lib/presentation/screens/search/widgets/search_field.dart
+++ lib/presentation/screens/search/widgets/search_field.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:github_app/extensions/build_context.dart';
 
 import '../../../../model/use_cases/profile_repo/search_repositories.dart';
 
@@ -13,14 +14,14 @@ final _searchFieldController = Provider.autoDispose((ref) {
   return controller;
 });
 
-final _hasText = Provider.autoDispose((ref) {
-  final searchController = ref.watch(_searchFieldController);
+final _focusNode = Provider.autoDispose((ref) {
+  final focusNode = FocusNode();
 
-  searchController.addListener(() {
-    ref.state = searchController.text.isNotEmpty;
+  ref.onDispose(() {
+    focusNode.dispose();
   });
 
-  return searchController.text.isNotEmpty;
+  return focusNode;
 });
 
 class SearchField extends ConsumerWidget {
@@ -30,29 +31,58 @@ class SearchField extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final searchFieldController = ref.watch(_searchFieldController);
     final searchKeywordNotifier = ref.watch(searchKeywordProvider.notifier);
-    final hasText = ref.watch(_hasText);
+    final focusNode = ref.watch(_focusNode);
 
-    return TextFormField(
-      controller: searchFieldController,
-      textInputAction: TextInputAction.search,
-      decoration: InputDecoration(
-        labelText: 'リポジトリを検索',
-        filled: true,
-        prefixIcon: const Icon(Icons.search),
-        suffixIcon: hasText
-            ? IconButton(
+    return Material(
+      color: context.colorScheme.surfaceVariant,
+      shape: const StadiumBorder(),
+      clipBehavior: Clip.antiAlias,
+      child: TextFormField(
+        controller: searchFieldController,
+        focusNode: focusNode,
+        textInputAction: TextInputAction.search,
+        maxLines: 1,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(8),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.transparent,
+            ),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.transparent,
+            ),
+          ),
+          disabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.transparent,
+            ),
+          ),
+          hintText: 'リポジトリを検索',
+          prefixIcon: const Icon(Icons.search, size: 24),
+          suffixIcon: ValueListenableBuilder(
+            valueListenable: searchFieldController,
+            builder: (_, value, child) {
+              if (value.text.isEmpty) return const SizedBox();
+
+              return IconButton(
                 onPressed: () {
                   searchFieldController.clear();
+
+                  focusNode.requestFocus();
                 },
                 icon: const Icon(Icons.cancel_outlined),
-              )
-            : null,
-      ),
-      onFieldSubmitted: (value) {
-        if (value.trim().isEmpty) return;
+              );
+            },
+          ),
+        ),
+        onFieldSubmitted: (value) {
+          if (value.trim().isEmpty) return;
 
-        searchKeywordNotifier.state = value.trim();
-      },
+          searchKeywordNotifier.state = value.trim();
+        },
+      ),
     );
   }
 }
