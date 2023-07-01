10d20fba9c6db24caa42d9b30847dc640ea280ec
Yudai Kitano <36891892+ktnydi@users.noreply.github.com>
2023-07-01 11:39:01 +0900

    feat: ローカライズ設定

--- lib/presentation/screens/app/app_screen.dart
+++ lib/presentation/screens/app/app_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import '../splash/splash_screen.dart';
 
@@ -9,6 +10,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ja'),
+      ],
       theme: ThemeData.from(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
