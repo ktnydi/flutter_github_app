be57701123f5ebc39681a6a83984bc3d9a6dc7e6
Yudai Kitano <36891892+ktnydi@users.noreply.github.com>
2023-07-07 07:27:46 +0900

    fix: アプリ起動後に設定した`ThemeMode`で表示するように修正

--- lib/model/use_cases/theme_mode.dart
+++ lib/model/use_cases/theme_mode.dart
@@ -9,7 +9,14 @@ final themeModeNotifierProvider =
 });
 
 class ThemeModeNotifier extends StateNotifier<ThemeMode> {
-  ThemeModeNotifier(this.ref) : super(ThemeMode.system);
+  ThemeModeNotifier(this.ref) : super(ThemeMode.system) {
+    final currentIndex = prefs.getInt(key);
+    if (currentIndex == null) return;
+
+    state = ThemeMode.values.firstWhere(
+      (element) => element.index == currentIndex,
+    );
+  }
 
   final Ref ref;
   final key = 'themeMode';
