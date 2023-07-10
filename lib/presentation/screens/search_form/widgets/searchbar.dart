d5455ab4efe28fce3a99d9b51c09977b75e59bab
Yudai Kitano <36891892+ktnydi@users.noreply.github.com>
2023-07-10 11:49:22 +0900

    feat: SearchbarのUIを変更

--- lib/presentation/screens/search_form/widgets/searchbar.dart
+++ lib/presentation/screens/search_form/widgets/searchbar.dart
@@ -31,124 +31,98 @@ class Searchbar extends ConsumerWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final visualDensity = Theme.of(context).visualDensity;
     final queryNotifier = ref.watch(searchRepoQueryProvider.notifier);
     final searchController = ref.watch(_searchbarController);
     final focusNode = ref.watch(_focusNode);
     final sortKeyNotifier = ref.watch(searchSortKeyProvider.notifier);
 
-    return Padding(
-      padding: EdgeInsets.only(
-        top: MediaQuery.of(context).viewPadding.top,
-      ),
+    return Material(
+      color: context.colorScheme.surfaceVariant,
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        height: kToolbarHeight,
-        child: Row(
-          children: [
-            Expanded(
-              child: Material(
-                color: context.colorScheme.surfaceVariant,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).viewPadding.top,
+        ),
+        child: SizedBox(
+          height: kToolbarHeight,
+          child: Row(
+            children: [
+              ConstrainedBox(
+                constraints: const BoxConstraints.tightFor(
+                  width: kToolbarHeight,
+                  height: kToolbarHeight,
                 ),
-                clipBehavior: Clip.antiAlias,
-                child: IntrinsicHeight(
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          controller: searchController,
-                          focusNode: focusNode,
-                          autofocus: true,
-                          textAlignVertical: TextAlignVertical.center,
-                          style: context.textTheme.bodyLarge,
-                          strutStyle: StrutStyle.fromTextStyle(
-                            context.textTheme.bodyLarge!,
-                            forceStrutHeight: true,
-                          ),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            filled: true,
-                            isDense: true,
-                            hintText: 'GitHub内のリポジトリを検索',
-                            suffixIconConstraints:
-                                visualDensity.effectiveConstraints(
-                              const BoxConstraints(
-                                minWidth: 0,
-                                minHeight: kMinInteractiveDimension,
-                              ),
-                            ),
-                            suffixIcon: ValueListenableBuilder(
-                              valueListenable: searchController,
-                              builder: (context, value, child) {
-                                if (value.text.isEmpty) {
-                                  return const SizedBox();
-                                }
-
-                                return IconButton(
-                                  onPressed: () {
-                                    searchController.clear();
-                                    focusNode.requestFocus();
-                                  },
-                                  icon: const Icon(Icons.cancel),
-                                  visualDensity: VisualDensity.compact,
-                                  color: context.colorScheme.primary,
-                                );
-                              },
-                            ),
-                          ),
-                          onFieldSubmitted: (value) {
-                            if (value.isEmpty) return;
-
-                            queryNotifier.state = value;
-                          },
+                child: const BackButton(),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: searchController,
+                        focusNode: focusNode,
+                        autofocus: true,
+                        textAlignVertical: TextAlignVertical.center,
+                        style: context.textTheme.bodyLarge,
+                        strutStyle: StrutStyle.fromTextStyle(
+                          context.textTheme.bodyLarge!,
+                          forceStrutHeight: true,
                         ),
-                      ),
-                      const VerticalDivider(
-                        width: 1,
-                        indent: 8,
-                        endIndent: 8,
-                      ),
-                      IconButton(
-                        onPressed: () async {
-                          final result = await showModalActionSheet(
-                            context: context,
-                            title: 'ソート方法',
-                            actions: SortKey.values.map((e) {
-                              return SheetAction(
-                                key: e,
-                                label: e.label,
-                              );
-                            }).toList(),
-                          );
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.fromLTRB(0, 16, 0, 8),
+                          isDense: true,
+                          hintText: 'Github内のリポジトリを検索',
+                        ),
+                        onFieldSubmitted: (value) {
+                          if (value.isEmpty) return;
 
-                          if (result == null) return;
-
-                          sortKeyNotifier.state = result;
+                          queryNotifier.state = value;
                         },
-                        icon: const Icon(Icons.sort_outlined),
                       ),
-                    ],
-                  ),
+                    ),
+                    ValueListenableBuilder(
+                      valueListenable: searchController,
+                      builder: (context, value, child) {
+                        if (value.text.isEmpty) {
+                          return const SizedBox();
+                        }
+
+                        return IconButton(
+                          onPressed: () {
+                            searchController.clear();
+                            focusNode.requestFocus();
+                          },
+                          icon: const Icon(Icons.clear),
+                        );
+                      },
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        final result = await showModalActionSheet(
+                          context: context,
+                          title: 'ソート方法',
+                          actions: SortKey.values.map((e) {
+                            return SheetAction(
+                              key: e,
+                              label: e.label,
+                            );
+                          }).toList(),
+                        );
+
+                        if (result == null) return;
+
+                        sortKeyNotifier.state = result;
+                      },
+                      icon: const Icon(Icons.sort_outlined),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(width: 16),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              style: TextButton.styleFrom(
-                shape: const BeveledRectangleBorder(),
-                padding: EdgeInsets.zero,
-                minimumSize: const Size(0, 40),
-                textStyle: context.textTheme.bodyLarge,
-              ),
-              child: const Text('閉じる'),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
